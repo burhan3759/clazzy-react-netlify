f91a0b28dc7569a4efefb7797d1f303b9add59c3
Mohamed Burhan <bern2727@gmail.com>
2021-08-15 23:01:20 +0800

    1. implement api

--- src/App.js
+++ src/App.js
@@ -1,20 +1,25 @@
 import React, { useState } from 'react';
-import myLogo from './assets/img/my_logo.png'
-import './App.css';
+import './University.css';
 
 import axios from 'axios'
 
 function App() {
 
   const [countryName, setCountryName] = useState('')
+  const [universities, setUniversities] = useState([])
 
   const getUniversityList = () => {
 
     console.log('countryName: ', countryName)
 
-    axios.get(`http://universities.hipolabs.com/search?country=${countryName}`)
+    axios.get(`http://universities.hipolabs.com/search`, {
+      params: {
+        country: countryName,
+      }
+    })
     .then(response => {
       console.log('res: ', response.data)
+      setUniversities(response.data)
     })
     .catch(err => {
       console.log('err: ', err)
@@ -28,13 +33,30 @@ function App() {
 
 
   return (
-    <div className="App">
+    <div className="main-container">
 
       <input type="text" onChange={getCountryName} />
 
-      <button onClick={getUniversityList}>
+      <button onClick={getUniversityList} className="search-btn">
         Get University List
       </button>
+
+      <div className="unilist-style">
+        {/* Loop the results */}
+        { universities.map((eachUni, key) => (
+            <div key={key} className="unilist-container-style">
+              {/* University Name */}
+              <span>{key+1}.</span>
+              <p>{eachUni.name}</p>
+              {
+                eachUni.web_pages.map((eachWebPage, key) => (
+                  <a href={eachWebPage} target="_blank" rel="noreferrer">{eachWebPage}</a>
+                ))
+              }
+            </div>
+          ))
+        }
+      </div>
     </div>
   );
 }
