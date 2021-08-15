ecc0dc14d6c6405b5b6d1813b275aa3610e980e3
Mohamed Burhan <bern2727@gmail.com>
2021-08-15 22:21:52 +0800

    1. added Sir

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          By Burhan
+          By Sir Burhan 
         </a>
       </header>
     </div>
